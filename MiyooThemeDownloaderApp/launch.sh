a9a5e9c57fc42c5724db6feeac76f504fcb9901e
potatochipbag <sebmau883@gmail.com>
2023-11-18 15:00:35 -0800

    Add files via upload

--- MiyooThemeDownloaderApp/launch.sh
+++ MiyooThemeDownloaderApp/launch.sh
@@ -2,7 +2,7 @@
 echo $0 $*
 cd $(dirname "$0")
 touch /tmp/stay_awake
-st -q -e sh /mnt/SDCARD/App/themestore/conv
+st -q -e sh /mnt/SDCARD/App/MiyooThemeDownloaderApp/conv
 #sleep 5
 echo $0 $*
 cd $(dirname "$0")
@@ -11,7 +11,7 @@ HOME=.
 ./mnt/SDCARD/App/themestore/DinguxCommander
 sh /mnt/SDCARD/App/Commander_Italic/launch.sh
 sync
-st -q -e sh /mnt/SDCARD/App/themestore/get
+st -q -e sh /mnt/SDCARD/App/MiyooThemeDownloaderApp/get
 echo $0 $*
 cd /mnt/SDCARD/.tmp_update
 ./bin/themeSwitcher
